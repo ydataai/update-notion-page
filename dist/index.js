4b4167cc82e194f7d791f9a51a2c13e6724f4a6f
Vasco Ramos <vasco.ramos@ydata.ai>
2022-11-11 11:40:32 +0000

    fix: input json handling (#10)

--- dist/index.js
+++ dist/index.js
@@ -7767,41 +7767,40 @@ const { Client } = __nccwpck_require__(324);
 
 async function run() {
   try {
-    const secretToken = core.getInput("notion_secret");
+    const secretToken = core.getInput("notion_secret", { required: true });
 
     const notion = new Client({
       auth: secretToken,
     });
 
-    const pageUpdateProperties = core.getInput("notion_page_update_body");
-    console.log(pageUpdateProperties);
+    const pageUpdateProperties = core.getInput("notion_page_update_properties", { required: true });
 
-    let pageId = core.getInput("notion_page_id");
-    const databaseId = core.getInput("notion_database_id");
-    const databaseQueryFilter = core.getInput("notion_database_query_filter");
+    let pageId = core.getInput("notion_page_id", { required: false });
+    const databaseId = core.getInput("notion_database_id", { required: false });
+    const databaseQueryFilter = core.getInput("notion_database_query_filter", { required: false });
 
     if (pageId === "") {
       if (databaseId === "" || databaseQueryFilter === "") {
-        core.setFailed("either pageId or (databaseId and databaseQueryFilter) must be provided");
+        throw new Error("Either a page ID or a database ID and query filter must be provided");
       }
 
       const databaseQueryResults = (
         await await notion.databases.query({
           database_id: databaseId,
-          filter: databaseQueryFilter,
+          filter: JSON.parse(databaseQueryFilter),
         })
       ).results;
 
       if (databaseQueryResults.length === 0) {
-        core.setFailed("page doesn't exist");
-      }
+        throw new Error("Could not find pages with filter: " + databaseQueryFilter);
+      } 
 
       pageId = databaseQueryResults[0].id;
     }
 
     await notion.pages.update({
       page_id: pageId,
-      properties: pageUpdateProperties,
+      properties: JSON.parse(pageUpdateProperties),
     });
   } catch (error) {
     core.setFailed(error.message);
